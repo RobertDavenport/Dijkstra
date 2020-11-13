6e8294f5b29942c214252ab5f382130a7844e92f
AlexanderLFloyd <32877587+AlexanderLFloyd@users.noreply.github.com>
2020-11-13 17:46:37 -0600

    Update routing.py

--- routing.py
+++ routing.py
@@ -1,37 +1,10 @@
 import sys
-import heapq
 
 
-class Node(object):
-    def __init__(self, name):
-        self._name = name
-        self._weights = {}
-        
-    def add_link(self, other, weight):
-        self._weights[other.name] = (other, weight)
-
-    @property
-    def name(self):
-        return self._name
-
-    # Returns a dictionary of this node's links.
-    # Each link is stored as a key,value pair of
-    # the linked node's name as the key, and a tuple
-    # of object representing the linked node, and
-    # the link's weight as the value.
-    def get_links(self):
-        return self._weights
-
-    def __str__(self):
-        return self._name
-
-
-# Returns a dictionary holding every node in the graph.
-# The dictionary is addressable by the name (letter)
-# representing a node.
 def create_graph(file):
-    graph = {}  # Stores the input data in a graph of nodes
     index = []  # This node at each index in the input adjacency matrix's header
+    nodes = []
+    dists = {}
     with open(file) as topology:
 
         # ================== Process header line
@@ -44,8 +17,6 @@ def create_graph(file):
         index.append("")
 
         for i in range(1, len(line)):
-            n = Node(line[i])
-            graph[line[i]] = n
             index.append(line[i])
 
         # Process until the end-of-file
@@ -57,73 +28,89 @@ def create_graph(file):
             line = line.split(',')
 
             if(len(line) > 1):  # Detects EOF
-                curr_node = graph[line[0]]
+                nodes.append(line[0])
+                dists[line[0]] = {}
                 for i in range(1, len(line)):
-                    curr_node.add_link(graph[index[i]], int(line[i]))
+                    dists[line[0]][index[i]] = int(line[i])
             else:
-                return graph
+                return nodes, dists
 
 
-# does not wok yet, need to review with Alex.
-def dijkstra(graph, start):
-    shortestEdge = 9999
-    for key, value in graph.items():
-        if (key == start):
-            # get edges
-            for edge in value.get_links:
-                if (edge < shortestEdge):
-                    shortestEdge = edge
-
-
-def dijkstra2(graph, start):
-    start = graph[start]
-    Q = []
-    for node in graph.values():
-        Q.append(node)
-    MAX_EDGE = 9999
+def dijkstra(nodes, dists, start):
+    MAX = 9999
     dist = {}
     prev = {}
+    pq = []
 
-    for node in graph.values():
-        dist[node.name] = MAX_EDGE
-        prev[node.name] = []
-    dist[start.name] = 0
+    for n in nodes:
+        dist[n] = MAX
+        prev[n] = []
+        pq.append(n)
 
-    u = None
-    ls = []
-    while(len(Q) > 0):
-        if(u is None):
-            u = Q[0]
-            Q.remove(u)
-        else:
-            u = min_dist(Q, dist, MAX_EDGE)
+    dist[start] = 0
+    while(len(pq) > 0):
+        u = min_dist(MAX, dist, pq)
+        pq.remove(u)
+        for node, weight in dists[u].items():
+            d = dist[u] + weight
+            if d <= dist[node]:
+                dist[node] = d
+                prev[node].append(u)
+                if(node == 'z' or u == 'z'):
+                    print(prev[node])
 
-        for edge in u.get_links().values():
-            alt = dist[u.name] + edge[1]
-            if (alt < dist[edge[0].name]):
-                dist[edge[0].name] = alt
-                prev[edge[0].name].append(u)
 
     return dist, prev
 
 
-def min_dist(Q, dist, MAX_EDGE):
-    minv = MAX_EDGE
-    ret = Q[0]
-    for node in Q:
-        if(dist[node.name] < minv):
-            minv = dist[node.name]
-            ret = node
-    Q.remove(ret)
-    return(ret)
+def min_dist(max, dist, q):
+    mind = max
+    minn = q[0]
+    for n in q:
+        if(dist[n] < mind):
+            mind = dist[n]
+            minn = n
+    return minn
+
+
+def path(prev,u):
+    print(u, prev[u])
+    print()
+
 
 # MAIN
-# Run dikstra algorithim on source csv and user specified start node
-dist,prev = dijkstra2(create_graph("topology-1.csv"), "u")
+# Run Dijkstra algorithm on source csv and user specified start node
+# dist, prev = dijkstra(create_graph("topology-1.csv"), "u")
 
-print("DIST: ",dist)
+nodes, dists = create_graph("topology-1.csv")
+#print(nodes)
+#print(dists)
+dist, prev = dijkstra(nodes, dists, "u")
+
+#for node in dist.keys():
+#    dist, prev = dijkstra2(create_graph("topology-1.csv"), node)
+#    print("Distance vector for node {}: {}".format(node, dist.values()))
+
+
+print("DIST: ", dist)
+for node in nodes:
+    path(prev, node)
+"""
 print("PREV:")
-for key, list in prev.items():
-    print(key)
-    for node in list:
-        print(node.name,end="\t")
+for u in nodes:
+    u2 = u
+    S = []
+    if (prev[u] is not None):
+        while u is not None:
+            S.append(u)
+            try:
+                u = prev[u].pop()
+            except:
+                break
+    print(u2, "::", S[::-1], end=" ")
+    tot = 0
+    for i in range(len(S) - 1):
+        links = graph[S[i]].get_links()
+        tot += links[S[i + 1]][1]
+    print(tot)"""
+
